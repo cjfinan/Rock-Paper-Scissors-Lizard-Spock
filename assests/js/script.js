36bb9d3a47f5b77e78408dd1b23954fb6709ab00
cjfinan <cjfinan98@gmail.com>
2022-08-30 13:06:35 +0000

    [ADD] runGame function idea

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,25 +1,26 @@
+
+
 let buttons = document.getElementsByClassName("btn")
 
 for(let button of buttons){
    button.addEventListener("click", userChoice)
 }
   
-function userChoice(){
+function userChoice(computerChoice){
     let userAnswer = this.getAttribute("id")
 console.log(this.getAttribute("id"))
-
-
-let randomNum = Math.floor(Math.random() * buttons.length);
-let randomChoice = buttons[randomNum]
-let randomChoiceId = randomChoice.getAttribute("id")
-console.log(randomChoiceId)
-
 }
 
 function computerChoice(){
-  
-    
-    
+    let randomNum = Math.floor(Math.random() * buttons.length);
+    let randomChoice = buttons[randomNum]
+    let randomChoiceId = randomChoice.getAttribute("id")
+    console.log(randomChoiceId)
 }
 
+function runGame(userAnswer){
+    if(userAnswer === "rock" && randomChoiceId === "rock"){
+        document.getElementById("game-result").innerHTML = "YOU ROCK"
+    }
+}
 
