6c1dbabf2c81cb0621ad8d6a8b06a266c468c658
cjfinan <cjfinan98@gmail.com>
2022-08-30 09:57:34 +0000

    [ADD] userChoice function

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,4 +1,18 @@
-function userChoice(){}
+
+document.addEventListener("DOMContentLoaded", userChoice)
+
+
+
+function userChoice(){
+    let buttons = document.getElementsByClassName("btn")
+console.log("test1")
+
+for(let button of buttons){
+    button.addEventListener("click", runGame)
+    let gameType = button.getAttribute("data-type");
+    runGame(gameType);
+    console.log(gameType)
+    }}
 
 function computerChice(){}
 
