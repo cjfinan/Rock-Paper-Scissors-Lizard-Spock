be92f01e0644017823047d1dbed908c3d9ffcc77
cjfinan <cjfinan98@gmail.com>
2022-09-01 09:49:10 +0000

    [FIX] put variables userScore and computerScore outside of if statement so they can be reached

--- assests/js/script.js
+++ assests/js/script.js
@@ -23,22 +23,29 @@ function runRound(){
     let userAnswer = this.getAttribute("id");
     console.log(userAnswer);
     
-    let roundOutcome = checkAnswer(userAnswer)
-    console.log(roundOutcome)
+    let roundOutcome = checkAnswer(userAnswer);
+    console.log(roundOutcome);
+
+    let userScore = parseInt(document.getElementById("user-score-number").innerHTML);
+    let computerScore = parseInt(document.getElementById("computer-score-number").innerHTML);
 
     if (roundOutcome === 1){
-        let userScore = parseInt(document.getElementById("user-score-number").innerHTML)
         document.getElementById("user-score-number").innerHTML = ++userScore;
-        document.getElementById("user-score-number").style.color = "rgb(32, 247, 90)"
-        document.getElementById("game-area-results").style.borderColor = "rgb(32, 247, 90)"
-        document.getElementById("game-area").style.borderColor = "rgb(32, 247, 90)"
-        setTimeout(userScoreChange, 500)
-     
-
-    }else if (roundOutcome === 2){
-        let computerScore = parseInt(document.getElementById("computer-score-number").innerHTML)
-    document.getElementById("computer-score-number").innerHTML = ++computerScore;
-    document.getElementById("computer-score-number").style.color = "red"
+        document.getElementById("user-score-number").style.color = "rgb(32, 247, 90)";
+        document.getElementById("game-area-results").style.borderColor = "rgb(32, 247, 90)";
+        document.getElementById("game-area").style.borderColor = "rgb(32, 247, 90)";
+        setTimeout(userScoreChange, 300);
+      }else if (roundOutcome === 2){
+        document.getElementById("computer-score-number").innerHTML = ++computerScore;
+        document.getElementById("computer-score-number").style.color = "rgb(224, 25, 18)";
+        document.getElementById("game-area-results").style.borderColor = "rgb(224, 25, 18)";
+        document.getElementById("game-area").style.borderColor = "rgb(224, 25, 18)";
+        setTimeout(computerScoreChange, 300);
+    } 
+    
+    
+    if (userScore === 5){
+       document.getElementById("game-result").innerHTML = `CONGRATULATIONS! you have beaten the computer! <br> The page will now refresh if you would like to try again!`
     }
 
 }
@@ -49,7 +56,7 @@ function checkAnswer(userAnswer){
     console.log(randomAnswer);
 
     if (randomAnswer === userAnswer){
-        document.getElementById("game-result").innerHTML = "Draw! Try Again"
+        document.getElementById("game-result").innerHTML = "Draw! Try Again";
         return 3;
     }
 
@@ -58,48 +65,48 @@ function checkAnswer(userAnswer){
 switch(userAnswer){
     case "rock":
         if ((randomAnswer === "lizard") || (randomAnswer === "scisscors")){
-            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`
+            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`;
             return 1;
         } else {
-            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`
+            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`;
             return 2;
         }
        
     case "paper":
         if ((randomAnswer === "rock") || (randomAnswer === "spock")){
-            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`
+            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`;
             return 1;
         } else {
-            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`
+            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`;
             return 2;
     
     }
 
     case "scissors":
         if ((randomAnswer === "paper") || (randomAnswer === "lizard")){
-            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`
+            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`;
             return 1;
         } else{
-            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`
+            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`;
             return 2;
         }
   
 
      case "lizard":
         if ((randomAnswer === "spock") || (randomAnswer === "paper")){
-            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`
+            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`;
             return 1;
         } else {
-            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`
+            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`;
             return 2;
         } 
 
     case "spock":
         if ((randomAnswer === "rock") || (randomAnswer === "scissors")){
-            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`
+            document.getElementById("game-result").innerHTML = `Well Done! ${userAnswer} beats ${randomAnswer}!`;
             return 1;
         } else {
-            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`
+            document.getElementById("game-result").innerHTML = `Unlucky! ${randomAnswer} beats ${userAnswer}!`;
             return 2;
         }
 }
@@ -108,12 +115,19 @@ switch(userAnswer){
 
 function rules(){
     rulesButton.style.height = "200px";
-    rulesButton.style.textAlign = "center"
-    rulesButton.innerHTML=`Click on a icon to play vs the computer! <br> The rules are as follows: Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors. Have FUN!`
+    rulesButton.style.textAlign = "center";
+    rulesButton.innerHTML=`Click on a icon to play vs the computer! <br> The rules are as follows: Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors. Have FUN!`;
 }
 
 function userScoreChange(){
-    document.getElementById("user-score-number").style.color = "white"
-    document.getElementById("game-area-results").style.borderColor = "white"
-    document.getElementById("game-area").style.borderColor = "white"
+    document.getElementById("user-score-number").style.color = "white";
+    document.getElementById("game-area-results").style.borderColor = "white";
+    document.getElementById("game-area").style.borderColor = "white";
 }
+
+function computerScoreChange(){
+    document.getElementById("computer-score-number").style.color = "white";
+    document.getElementById("game-area-results").style.borderColor = "white";
+    document.getElementById("game-area").style.borderColor = "white";
+}
+
